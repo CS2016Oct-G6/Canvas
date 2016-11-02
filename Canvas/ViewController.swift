536b621cc2cea869979b07dbfe48aabd02d7546b
Nguyen the phuong <nguyenphuong.it242@gmail.com>
2016-11-02 19:38:55 +0700

    milestone 2

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        trayOriginalCenter = trayView.center
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        
+        let point = panGestureRecognizer.location(in: self.view)
+        let translation = panGestureRecognizer.translation(in: self.view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.began {
+            print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
 
 }
 
